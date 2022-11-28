73f139a81da9a689c5dd678e51f69f0d1facb059
Sergei Krylov <sergei.krylov@yahoo.com>
2022-11-28 19:55:03 +0100

    BOSA21Q1-922 Always check all views when run erblint (keep 0.27 version of the script)

--- .github/run_erblint.sh
+++ .github/run_erblint.sh
@@ -2,20 +2,7 @@
 
 shopt -s globstar
 
-CURRENT_BRANCH=$(git branch --show-current)
-DEFAULT_BRANCH=$(basename $(git symbolic-ref refs/remotes/origin/HEAD --short))
-
-ALL_VIEWS=decidim**/app/{cells,views}/**/*.erb
-MODIFIED_VIEWS=$(git diff --name-only "${DEFAULT_BRANCH}" | grep -E '(views|cells)/.*\.erb$')
-
-# Scan only the modified ERB files, except for the default branch
-FILES=$([[ "${CURRENT_BRANCH}" == "${DEFAULT_BRANCH}" ]] && echo ${ALL_VIEWS} || echo ${MODIFIED_VIEWS})
-
-if [[ -n "$FILES" ]] ; then
-  bundle exec erblint ${FILES}
-else
-  echo "No ERB files changed"
-fi
+bundle exec erblint app/{cells,views}/**/*.erb
 
 # Store the return code of the erblint execution
 EXIT_CODE=$?
