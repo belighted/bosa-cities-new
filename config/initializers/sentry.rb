b07692b1907d528118e3156518b01db13066ba78
Sergei Krylov <sergei.krylov@yahoo.com>
2022-12-07 11:53:08 +0100

    BOSA21Q1-830 Fix sentry env for production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 Sentry.init do |config|
   config.logger = Sentry::Logger.new($stdout)
   config.dsn = ENV.fetch("SENTRY_DSN", nil)
-  config.enabled_environments = %w(bosa-cities-new-production bosa-cities-new-uat)
+  config.enabled_environments = %w(bosa-cities-new bosa-cities-new-uat)
   config.environment = ENV.fetch("SENTRY_CURRENT_ENV", "missing-env")
   config.send_default_pii = true
 end
